8942094ae36a4c14317e6b4f37296ad6e9a00468
tomaszpankowski <--get-all>
2022-05-22 22:46:53 +0200

    layout

---
+++ src/app/trainers/trainers-s1/trainers-s1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trainers-s1',
+  templateUrl: './trainers-s1.component.html',
+  styleUrls: ['./trainers-s1.component.scss']
+})
+export class TrainersS1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
